08dc869352b103a54fe04dea5a808d94674fe752
Krish <surendra.nath@urumitech.com>
2021-02-12 22:44:24 +0000

    Initial Functions

--- Moneybox.App/Domain/Account.cs
+++ Moneybox.App/Domain/Account.cs
@@ -6,6 +6,11 @@ namespace Moneybox.App
     {
         public const decimal PayInLimit = 4000m;
 
+        public Account(Guid id, decimal balance)
+        {
+            Id = id;
+            Balance = balance;
+        }
         public Guid Id { get; set; }
 
         public User User { get; set; }
@@ -13,7 +18,25 @@ namespace Moneybox.App
         public decimal Balance { get; set; }
 
         public decimal Withdrawn { get; set; }
+        public decimal Paid { get; set; }
 
         public decimal PaidIn { get; set; }
+
+
+        public decimal Credit(decimal amount)
+        {
+            if (amount > PayInLimit)
+            {
+                throw new InvalidOperationException("Account pay in limit reached");
+            }
+            Balance += amount;
+            PaidIn += amount;
+            return Balance;
+        }
+
+        public bool CanCredit(decimal amount)
+        {
+            return PaidIn + amount <= PayInLimit;
+        }
     }
 }
